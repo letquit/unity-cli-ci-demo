f316a8675fcff7909ad54def22a1fc8d8c6c92c2
letquit <1446444589@qq.com>
2026-09-22 22:26:37 +0800

    Initial Unity project with EditMode smoke test

---
+++ Assets/Tests/Tests/SmokeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace  UnityCliCiDemo.Tests
+{
+    public class SmokeTests
+    {
+        [Test]
+        public void OnePlusOne_EqualsTwo()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
